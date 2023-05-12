9e0710f1ef78ac606f8829e93d77f0f35d617ce3
M0ngi <saidanemongi@gmail.com>
2023-05-12 01:12:21 +0100

    Add arguments

--- main.go
+++ main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/chopper-c2-framework/c2-chopper/core/plugins"
 )
 
-type EvilPlugin plugins.Plugin
+type EvilPlugin struct {
+	plugins.Plugin
+	targetIp string
+}
 
-func  New() plugins.Plugin {
-	return plugins.Plugin{
-		Name: "EvilPlugin",
+func (p EvilPlugin) MetaInfo() *plugins.Metadata {
+	return &p.Metadata
+}
+
+func (p EvilPlugin) Info() *plugins.PluginInfo {
+	return &p.PluginInfo
+}
+
+func (p EvilPlugin) Options() map[string]string {
+	return p.PluginInfo.Options
+}
+
+func (p *EvilPlugin) SetArgs(args ...interface{}) error {
+	fmt.Println("Setting args")
+
+	arg1, ok := args[0].(string)
+	if !ok {
+		return errors.New("Bad first argument")
+	}
+
+	fmt.Println("Setting targetIp to", arg1)
+	p.targetIp = fmt.Sprint(arg1)
+	return nil
+}
+
+func (p EvilPlugin) Exploit(args ...interface{}) []byte {
+	// Do evil things with the args
+	fmt.Println("Do evil things to", p.targetIp)
+	return []byte(fmt.Sprint("EvilPlugin attacking ", p.targetIp))
+}
+
+var Instance EvilPlugin = EvilPlugin{
+	Plugin: plugins.Plugin{
 		Metadata: plugins.Metadata{
 			Version:     "1.0",
 			Author:      "Evil Corp",
@@ -21,32 +55,12 @@ func  New() plugins.Plugin {
 			Description: "This plugin does evil things",
 		},
 		PluginInfo: plugins.PluginInfo{
+			Name: "EvilPlugin",
 			Options: map[string]string{
 				"target": "string",
 			},
 			ReturnType: "bytes",
 		},
-	}
-}
-
-func (p *EvilPlugin) MetaInfo() *plugins.Metadata {
-	return &p.Metadata
-}
-
-func (p *EvilPlugin) Info() *plugins.PluginInfo {
-	return &p.PluginInfo
-}
-
-func (p *EvilPlugin) Options() map[string]string {
-	return p.PluginInfo.Options
-}
-
-func (p *EvilPlugin) SetArgs(args ...interface{}) {
-	fmt.Println("Setting args")
-
-}
-
-func (p *EvilPlugin) Exploit(args ...interface{}) []byte {
-	// Do evil things with the args
-	return []byte("EvilPlugin has done its dirty work")
+	},
+	targetIp: "",
 }
